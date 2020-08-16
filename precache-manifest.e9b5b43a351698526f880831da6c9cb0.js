0c2cdd9f3d1b8cf8075fd3691fffb2673359be8c
Jorge Luis Rojas Arcos <jorgelroar@gmail.com>
2020-08-16 17:38:17 -0500

    Updates

---
+++ precache-manifest.e9b5b43a351698526f880831da6c9cb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a708504d4521b1054f1fdb62b334ee0",
+    "url": "/thermo/index.html"
+  },
+  {
+    "revision": "f55f47eaf8ff8e6d1537",
+    "url": "/thermo/static/css/main.c0c614a9.chunk.css"
+  },
+  {
+    "revision": "a90d32677197d0cfc88e",
+    "url": "/thermo/static/js/2.a68341dc.chunk.js"
+  },
+  {
+    "revision": "0fce063a4ec3cb005e7265693b407036",
+    "url": "/thermo/static/js/2.a68341dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55f47eaf8ff8e6d1537",
+    "url": "/thermo/static/js/main.5f0e9dc5.chunk.js"
+  },
+  {
+    "revision": "1bcfcf217c9ae856c25b",
+    "url": "/thermo/static/js/runtime-main.84c5dc03.js"
+  }
+]);
